020161fa97a9f4b8f27b7b0c4dcb2f15b24c254f
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2021-07-12 12:10:47 -0500

    Act

--- instalacion/base_de_datos.sql
+++ instalacion/base_de_datos.sql
@@ -1,14 +1,4 @@
 -- creacion de base de datos-.
-
-DROP TABLE IF EXISTS tb_palabras;
-CREATE TABLE tb_palabras
-(
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    fecha_registro DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP, 
-    palabra NVARCHAR(50) NOT NULL,
-    idioma TINYINT NOT NULL -- llave foranea.
-);
-
 DROP TABLE IF EXISTS tb_idiomas;
 CREATE TABLE tb_idiomas
 (
@@ -17,12 +7,15 @@ CREATE TABLE tb_idiomas
 );
 INSERT tb_idiomas VALUES('1', 'ESPAÑOL'),('2','TUCANO'); -- Insertar datos por defecto
 
-DROP TABLE IF EXISTS tb_traducciones;
-CREATE TABLE tb_traducciones
+DROP TABLE IF EXISTS tb_diccionario;
+CREATE TABLE tb_diccionario
 (
-    id INT PRIMARY KEY AUTO_INCREMENT,
-    id_palabra INT NOT NULL,  -- llave foranea.
-    id_palabra_traduccion INT NOT NULL -- llave foranea.
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	palabra VARCHAR(60) NOT NULL,
+	palabra_idioma TINYINT NOT NULL, -- Llave foranea
+	traduccion VARCHAR(60) NOT NULL, 
+	traduccion_idioma TINYINT NOT NULL, -- lLave foranra
+	significado VARCHAR(300) NOT NULL
 );
 
 DROP TABLE IF EXISTS tb_vocabularios;
@@ -32,61 +25,34 @@ CREATE TABLE tb_vocabularios
     frase NVARCHAR(200) NOT NULL,  
     frase_idioma TINYINT NOT NULL, -- llave foranea.
     traduccion  NVARCHAR(200) NOT NULL,
-    traduccion_idioma TINYINT NOT NULL -- llave foranea.
+    traduccion_idioma TINYINT NOT NULL, -- llave foranea.
+	signifcado VARCHAR(300) NOT NULL
 );
 
 -- FUNCIONES
 
 -- Funcion de insertar palabras.
-
 DROP FUNCTION IF EXISTS fun_agregar_palabra;
-delimiter //
-CREATE FUNCTION fun_agregar_palabra( v_palabra NVARCHAR(50), v_idioma TINYINT)
-RETURNS bit
-BEGIN
-	DECLARE validacion_existencia INT;
-	SET validacion_existencia = (SELECT  COUNT(*) FROM tb_palabras WHERE palabra = v_palabra AND idioma = v_idioma);
-	if (validacion_existencia = 0) then 
-		INSERT INTO tb_palabras (palabra, idioma) VALUES (v_palabra, v_idioma);  
+DELIMITER //
+CREATE FUNCTION fun_agregar_palabra(p_palabra VARCHAR(60), p_palabra_idioma TINYINT, p_traduccion VARCHAR(60), p_traduccion_idioma TINYINT)
+RETURNS BIT
+BEGIN 
+	DECLARE v BIT;
+	SET v = IF((SELECT COUNT(*) FROM tb_diccionario t1 WHERE t1.palabra = p_palabra AND t1.palabra_idioma = p_palabra_idioma) = 0, 1, 0);
+	IF (v = 1) then
+		INSERT INTO tb_diccionario(palabra, palabra_idioma, traduccion, traduccion_idioma)
+		VALUES(UPPER(p_palabra), p_palabra_idioma, UPPER(p_traduccion), p_traduccion_idioma); 
 		RETURN 1;
-		
-	ELSE 
+	ELSE
 		RETURN 0;
-	END if ; 
-	 
+	END IF;
 END //
-delimiter ;
-
--- Función de insertar traducciones.
-
-DROP FUNCTION IF EXISTS fun_agregar_traduccion;
-delimiter //
-CREATE FUNCTION fun_agregar_traduccion( v_id_palabra INT, v_id_palabra_traduccion INT)
-RETURNS bit
-BEGIN
-	
-	DECLARE validacion_1 INT;
-	DECLARE validacion_2 INT;
-	DECLARE validacion_3 INT;
-	
-	SET validacion_1 = (SELECT COUNT(*) FROM tb_palabras WHERE id = v_id_palabra);
-	SET validacion_2 = (SELECT COUNT(*) FROM tb_palabras WHERE id = v_id_palabra_traduccion);
-	SET validacion_3 = (SELECT COUNT(*) FROM tb_traducciones WHERE id_palabra = v_id_palabra AND id_palabra_traduccion = v_id_palabra_traduccion );
-	
-	
-	if (validacion_1 = 1 AND validacion_2 = 1 AND validacion_3 = 0) then
-		INSERT INTO tb_traducciones(id_palabra, id_palabra_traduccion) VALUES(v_id_palabra, v_id_palabra_traduccion); 
-		RETURN 1;
-	else
-		RETURN 0;
-	END if;
-END //
-delimiter ;
+DELIMITER ;
 
 -- Agregar vocabulario.
-DROP FUNCTION IF EXISTS fu_agregar_vocabulario;
+DROP FUNCTION IF EXISTS fun_agregar_vocabulario;
 DELIMITER //
-CREATE FUNCTION fu_agregar_vocabulario(p_frase VARCHAR(200), p_frase_idioma TINYINT, p_traduccion VARCHAR(200), p_traduccion_idioma TINYINT)
+CREATE FUNCTION fun_agregar_vocabulario(p_frase VARCHAR(200), p_frase_idioma TINYINT, p_traduccion VARCHAR(200), p_traduccion_idioma TINYINT)
 RETURNS BIT
 BEGIN
 	DECLARE v1 BIT;
@@ -102,20 +68,6 @@ BEGIN
 	END IF;
 END //
 DELIMITER ;
-
-
-
-
--- vista
-
-DROP TABLE IF EXISTS vista_traductor;
-
-CREATE VIEW vista_traductor    
-AS 
-SELECT COUNT( * ) AS conteo, t1.palabra, t2.idioma
-from tb_palabras t1, tb_idiomas t2
-WHERE t1.id = t2.id
-GROUP BY t2.idioma, t1.palabra ;
 
 
 
