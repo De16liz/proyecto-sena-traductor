afa60edf636b97f77886c5a14b32a89d9cfa6840
HeilerNova <81369584+HeilerNova@users.noreply.github.com>
2021-07-12 12:14:08 -0500

    Create valores_iniciales.sql

---
+++ instalacion/valores_iniciales.sql
@@ -0,0 +1,9 @@
+SELECT fun_agregar_vocabulario('HOLA COMO ESTAN','1','NEE WAKATI MUSA','2','');
+SELECT fun_agregar_vocabulario('YO VOY AL PUERTO','1','YUU WAAGU WEEE','2','');
+SELECT fun_agregar_vocabulario('VAMOS AL RIO','1','WAAGU WAA DIA','2','');
+
+SELECT fun_agregar_palabra('Palabra','1','Traduccion','2','Significado');
+SELECT fun_agregar_palabra('no tengo','1','wamé','2','');
+SELECT fun_agregar_palabra('saber','1','masi','2','');
+SELECT fun_agregar_palabra('hacer mas ','1','nemo','2','');
+SELECT fun_agregar_palabra('su casa','1','cú yá wii','2','se refiere a la casa del hombre');
