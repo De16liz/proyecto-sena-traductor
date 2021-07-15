b78bf344fe636cabb29e2e11eddc7b49f5f24df3
De16liz <deligodi1628@gmail.com>
2021-07-15 10:36:50 -0500

    modificacion

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
     require_once 'libs/base-de-datos.php';
 
 
-    $d = new BaseDeDatos('LocalHost', 'root' , '', 'traductos');
+    $d = new BaseDeDatos('LocalHost', 'root' , '', 'traductor');
 
     $R =  $d->ejecutar_consulta("Select * from tb_diccionario");
 
